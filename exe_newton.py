57304d02699eef602e3e29ccc21ccce51603ab41
Jun_tsune <junya_tsuneishi@berkeley.edu>
2024-08-27 17:00:57 -0700

    repaire some mistakes

--- exe_newton.py
+++ exe_newton.py
@@ -1,7 +1,7 @@
 import newton
 import numpy as np
 
-x = np.asarray([1,2,3]) #starting point
+x = np.asarray([1.0,2.0,3.0]) #starting point
 
 def est_func(x):# assuming func y=x^2+y^3+z^2+xyz
     y = x[0]**2+x[1]**3+x[2]**2+x[0]*x[1]*x[2]
--- newton.py
+++ newton.py
@@ -1,32 +1,34 @@
-def derivative(est_func, x, est_precision, sign):
+import numpy as np
+
+def derivative(est_func, x, dev_eps, sign):
     """Caluculating first derivative.
 
     Keyword arguments:
     est_func -- function of research (def)
     x -- point of derivative (int, float)
-    est_precision -- search area (int, float)
+    dev_eps -- epsilon for derivative (float)
     sin -- derivative orientation (1 or -1)
 
     Return:
     dev -- an derivative number (int, float)
     """
-    dev = x
+    dev = x.copy()
     for i in range(len(x)):
-        x_2 = x
-        x_2[i] = x[i] + est_precision * sign
+        x_2 = x.copy()
+        x_2[i] = x[i] + dev_eps * sign
         y = est_func(x)
         y_2 = est_func(x_2)
-        dev[i] = (y_2 - y)/(est_precision * sign)
+        dev[i] = (y_2 - y)/(dev_eps * sign)
     return dev 
 
 
-def sec_derivative(est_func, x, est_precision, sign):
+def sec_derivative(est_func, x, dev_eps, sign):
     """Caluculating second derivative.
 
     Keyword arguments:
     est_func -- function of research (def)
     x -- point of derivative (int, float)
-    est_precision -- search area (int, float)
+    dev_eps -- epsilon for derivative (float)
     sin -- derivative orientation (1 or -1)
 
     Return:
@@ -34,20 +36,21 @@ def sec_derivative(est_func, x, est_precision, sign):
     """
     dev = np.asarray([x,x,x])
     for i in range(len(x)):
-        x_2 = x
-        x_2[i] = x[i] + est_precision * sign
-        y = derivative(est_func, x, est_precision, sign)
-        y_2 = derivative(est_func, x_2, est_precision, sign)
-        dev[i] = (y_2 - y)/est_precision * sign
+        x_2 = x.copy()
+        x_2[i] = x[i] + dev_eps * sign
+        y = derivative(est_func, x, dev_eps, sign)
+        y_2 = derivative(est_func, x_2, dev_eps, sign)
+        dev[i] = (y_2 - y)/dev_eps * sign
     return dev
 
 
-def optimize(est_func, x, threshold = 10**(-6), max_iter=10000):
+def optimize(est_func, x, dev_eps = 10**(-6), threshold = 10**(-4), max_iter=10000):
     """Caluculating optimized points.
 
     Keyword arguments:
     est_func -- function of research (def)
     x -- starting point of optimization (int, float)
+    dev_eps -- epsilon for derivative (float)
     threshold -- threshold of OK (int, float)
     max_iter -- how many times to try optimazation (int, default 10000)
 
@@ -55,12 +58,12 @@ def optimize(est_func, x, threshold = 10**(-6), max_iter=10000):
     x -- an optimized number (int, float)
     """
     for i in range(max_iter):
-        est_precision = 1/((i+1)*100)
         sign = (-1)**i
-        dev = derivative(est_func, x, est_precision, sign)
-        sec_dev = sec_derivative(est_func, x, est_precision, sign)
-        x = x - np.linalg.inv(a)*sec_dev
-        if np.linalg.norm(np.linalg.inv(a)*sec_dev, ord=2) < threshold:
+        dev = derivative(est_func, x, dev_eps, sign)
+        sec_dev = sec_derivative(est_func, x, dev_eps, sign)
+        dif = np.dot(np.linalg.inv(sec_dev), dev.T)
+        x = x - dif
+        if np.linalg.norm(dif) < threshold:
             break
     if i == max_iter-1:
         print("max_itter may be small")    
